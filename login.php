099d4d5b4d628e25e726eeac2f09b296b8e3ce52
gurifisu1 <76419116+gurifisu1@users.noreply.github.com>
2021-03-04 10:47:52 +0900

    Update and rename Login.php to login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes">
+	<meta charset="utf-8">
+	<title>ログインページ</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1><img src="iconb.png" width="40px">  ChemSquareβ</h1>
+<hr>
+
+<h2>ログインページ</h2>
+
+<form action = "" method = "post" name = "login_form">
+メールアドレス：<input type = "text" name = "mail" placeholder = "メールアドレス"><br>
+パスワード：　　<input type = "password" name = "pass" placeholder = "パスワード"><br>
+<input type = "submit" name = "login" value = "ログイン">
+</form>
+初めての方は<a href="https://tb-221172.tech-base.net/preregist.php">こちら</a><br>
+
+<hr>
+<!-- トップページへのリンク -->
+<a href="https://tb-221172.tech-base.net/top.html"> ChemSquareトップ </a>
+
+</body>
+</html>
